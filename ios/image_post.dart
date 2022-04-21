8f64402da2db7089075e5dc97299b6109a85bb3c
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 17:54:44 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -91,8 +91,4 @@ class _ImagePost extends State<ImagePost> {
 
   bool showHeart = false;
 
-  TextStyle boldStyle = TextStyle(
-    color: Colors.black,
-    fontWeight: FontWeight.bold,
-  );
-
+  TextStyle boldStyle = Te
