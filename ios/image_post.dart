912f814a2378e3c376f40e622b3232c9cdc84542
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 19:28:33 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -34,7 +34,7 @@ class ImagePost extends StatefulWidget {
     return ImagePost(
       username: data['username'],
       location: data['location'],
-      mediaUrl
+      mediaUr
       
     
        
