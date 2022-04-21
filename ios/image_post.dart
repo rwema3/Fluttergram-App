df5a7193653e5314c8a7dbef39b8371eddbd8947
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 20:12:33 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -21,6 +21,7 @@ class ImagePost extends StatefulWidget {
   factory ImagePost.fromDocument(DocumentSnapshot document) {
     return ImagePost(
       username: document['username'],
+      location: document['location'],
       
        
       
