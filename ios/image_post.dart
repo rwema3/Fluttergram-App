f06297860e1fa194fe5b561957ce0d142d54de9a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 16:01:10 +0200

    release and re assert

---
+++ ios/image_post.dart
@@ -0,0 +1,361 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'main.dart';
+import 'dart:async';
+import 'profile_page.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'comment_screen.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class ImagePost extends StatefulWidget {
+  const ImagePost(
+      {this.mediaUrl,
+      this.username,
+      this.location,
+      this.description,
+      this.likes,
+      this.postId,
+      this.ownerId});
+
+  factory ImagePost.fromDocument(DocumentSnapshot document) {
+    return ImagePost(
+      username: document['username'],
+      location: document['location'],
+      mediaUrl: document['mediaUrl'],
+      likes: document['likes'],
+      description: document['description'],
+      postId: document.id,
+      ownerId: document['ownerId'],
+    );
+  }
+
+  factory ImagePost.fromJSON(Map data) {
+    return ImagePost(
+      username: data['username'],
+      location: data['location'],
+      mediaUrl: data['mediaUrl'],
+      likes: data['likes'],
+      description: data['description'],
+      ownerId: data['ownerId'],
+      postId: data['postId'],
+    );
+  }
+
+  int getLikeCount(var likes) {
+    if (likes == null) {
+      return 0;
+    }
+// issue is below
+    var vals = likes.values;
+    int count = 0;
+    for (var val in vals) {
+      if (val == true) {
+        count = count + 1;
+      }
+    }
+
+    return count;
+  }
+
+  final String mediaUrl;
+  final String username;
+  final String location;
+  final String description;
+  final likes;
+  final String postId;
+  final String ownerId;
+
+  _ImagePost createState() => _ImagePost(
+        mediaUrl: this.mediaUrl,
+        username: this.username,
+        location: this.location,
+        description: this.description,
+        likes: this.likes,
+        likeCount: getLikeCount(this.likes),
+        ownerId: this.ownerId,
+        postId: this.postId,
+      );
+}
+
+class _ImagePost extends State<ImagePost> {
+  final String mediaUrl;
+  final String username;
+  final String location;
+  final String description;
+  Map likes;
+  int likeCount;
+  final String postId;
+  bool liked;
+  final String ownerId;
+
+  bool showHeart = false;
+
+  TextStyle boldStyle = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+  var reference = FirebaseFirestore.instance.collection('insta_posts');
+
+  _ImagePost(
+      {this.mediaUrl,
+      this.username,
+      this.location,
+      this.description,
+      this.likes,
+      this.postId,
+      this.likeCount,
+      this.ownerId});
+
+  GestureDetector buildLikeIcon() {
+    Color color;
+    IconData icon;
+
+    if (liked) {
+      color = Colors.pink;
+      icon = FontAwesomeIcons.solidHeart;
+    } else {
+      icon = FontAwesomeIcons.heart;
+    }
+
+    return GestureDetector(
+        child: Icon(
+          icon,
+          size: 25.0,
+          color: color,
+        ),
+        onTap: () {
+          _likePost(postId);
+        });
+  }
+
+  GestureDetector buildLikeableImage() {
+    return GestureDetector(
+      onDoubleTap: () => _likePost(postId),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          CachedNetworkImage(
+            imageUrl: mediaUrl,
+            fit: BoxFit.fitWidth,
+            placeholder: (context, url) => loadingPlaceHolder,
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+          showHeart
+              ? Positioned(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child:  Opacity(
+                        opacity: 0.85,
+                        child: FlareActor("assets/flare/Like.flr",
+                          animation: "Like",
+                        )),
+                  ),
+                )
+              : Container()
+        ],
+      ),
+    );
+  }
+
+  buildPostHeader({String ownerId}) {
+    if (ownerId == null) {
+      return Text("owner error");
+    }
+
+    return FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection('insta_users')
+            .doc(ownerId)
+            .get(),
+        builder: (context, snapshot) {
+
+          if (snapshot.data != null) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(snapshot.data.data()['photoUrl']),
+                backgroundColor: Colors.grey,
+              ),
+              title: GestureDetector(
+                child: Text(snapshot.data.data()['username'], style: boldStyle),
+                onTap: () {
+                  openProfile(context, ownerId);
+                },
+              ),
+              subtitle: Text(this.location),
+              trailing: const Icon(Icons.more_vert),
+            );
+          }
+
+          // snapshot data is null here
+          return Container();
+        });
+  }
+
+  Container loadingPlaceHolder = Container(
+    height: 400.0,
+    child: Center(child: CircularProgressIndicator()),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    liked = (likes[googleSignIn.currentUser.id.toString()] == true);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        buildPostHeader(ownerId: ownerId),
+        buildLikeableImage(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(padding: const EdgeInsets.only(left: 20.0, top: 40.0)),
+            buildLikeIcon(),
+            Padding(padding: const EdgeInsets.only(right: 20.0)),
+            GestureDetector(
+                child: const Icon(
+                  FontAwesomeIcons.comment,
+                  size: 25.0,
+                ),
+                onTap: () {
+                  goToComments(
+                      context: context,
+                      postId: postId,
+                      ownerId: ownerId,
+                      mediaUrl: mediaUrl);
+                }),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$likeCount likes",
+                style: boldStyle,
+              ),
+            )
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                margin: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  "$username ",
+                  style: boldStyle,
+                )),
+            Expanded(child: Text(description)),
+          ],
+        )
+      ],
+    );
+  }
+
+  void _likePost(String postId2) {
+    var userId = googleSignIn.currentUser.id;
+    bool _liked = likes[userId] == true;
+
+    if (_liked) {
+      print('removing like');
+      reference.doc(postId).update({
+        'likes.$userId': false
+        //firestore plugin doesnt support deleting, so it must be nulled / falsed
+      });
+
+      setState(() {
+        likeCount = likeCount - 1;
+        liked = false;
+        likes[userId] = false;
+      });
+
+      removeActivityFeedItem();
+    }
+
+    if (!_liked) {
+      print('liking');
+      reference.doc(postId).update({'likes.$userId': true});
+
+      addActivityFeedItem();
+
+      setState(() {
+        likeCount = likeCount + 1;
+        liked = true;
+        likes[userId] = true;
+        showHeart = true;
+      });
+      Timer(const Duration(milliseconds: 2000), () {
+        setState(() {
+          showHeart = false;
+        });
+      });
+    }
+  }
+
+  void addActivityFeedItem() {
+    FirebaseFirestore.instance
+        .collection("insta_a_feed")
+        .doc(ownerId)
+        .collection("items")
+        .doc(postId)
+        .set({
+      "username": currentUserModel.username,
+      "userId": currentUserModel.id,
+      "type": "like",
+      "userProfileImg": currentUserModel.photoUrl,
+      "mediaUrl": mediaUrl,
+      "timestamp": DateTime.now(),
+      "postId": postId,
+    });
+  }
+
+  void removeActivityFeedItem() {
+    FirebaseFirestore.instance
+        .collection("insta_a_feed")
+        .doc(ownerId)
+        .collection("items")
+        .doc(postId)
+        .delete();
+  }
+}
+
+class ImagePostFromId extends StatelessWidget {
+  final String id;
+
+  const ImagePostFromId({this.id});
+
+  getImagePost() async {
+    var document =
+        await FirebaseFirestore.instance.collection('insta_posts').doc(id).get();
+    return ImagePost.fromDocument(document);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getImagePost(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Container(
+                alignment: FractionalOffset.center,
+                padding: const EdgeInsets.only(top: 10.0),
+                child: CircularProgressIndicator());
+          return snapshot.data;
+        });
+  }
+}
+
+void goToComments(
+    {BuildContext context, String postId, String ownerId, String mediaUrl}) {
+  Navigator.of(context)
+      .push(MaterialPageRoute<bool>(builder: (BuildContext context) {
+    return CommentScreen(
+      postId: postId,
+      postOwner: ownerId,
+      postMediaUrl: mediaUrl,
+    );
+  }));
+}
