d6e2a2a19183e0dd597750b4beafeaee6ab96c96
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 17:49:46 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -181,7 +181,4 @@ class _ImagePost extends State<ImagePost> {
               title: GestureDetector(
                 child: Text(snapshot.data.data()['username'], style: boldStyle),
                 onTap: () {
-                  openProfile(context, ownerId);
-                },
-              ),
 
