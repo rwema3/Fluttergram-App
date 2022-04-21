4179a6f60b0ecbc531ee52b5c4a517171019995a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 20:11:12 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'    
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+    
     
        
       
