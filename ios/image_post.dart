674390608df481d1ee8c89fb97a1c3abd21e8ea5
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 17:49:51 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -179,5 +179,4 @@ class _ImagePost extends State<ImagePost> {
                 backgroundColor: Colors.grey,
               ),
               title: GestureDetector(
-                child: Text(snapshot.data.data()['username'], style: boldStyle),
 
