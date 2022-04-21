a51444385790494446e87db47f47bf98526f7f58
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 19:38:26 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -4,7 +4,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'main.dart';
 import 'dart:async';
 import 'profile_page.dart';
-import 'package:cached_network_image/cached_network_image.dart';
     
     
        
