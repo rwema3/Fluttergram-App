07f9fefcea95917d91391efa793ffc6d1953bd0d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-04-21 20:48:08 +0200

    Update image_post.dart

--- ios/image_post.dart
+++ ios/image_post.dart
@@ -176,4 +176,6 @@ class _ImagePost extends State<ImagePost> {
             return ListTile(
               leading: CircleAvatar(
                 backgroundImage: CachedNetworkImageProvider(snapshot.data.data()['photoUrl']),
+                backgroundColor: Colors.grey,
+              ),
 
