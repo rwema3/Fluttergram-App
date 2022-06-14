f4f38e1fb91289bd691278eefe26eac4c8f313b8
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-14 08:20:39 +0200

    Update edit_profile_page.dart

--- lib/models/edit_profile_page.dart
+++ lib/models/edit_profile_page.dart
@@ -33,23 +33,7 @@ class EditProfilePage extends StatelessWidget {
         .doc(currentUserModel.id)
         .update({
       "displayName": nameController.text,
-      "bio": bioController.text,
-    });
-  }
-
-  Widget buildTextField({String name, TextEditingController controller}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 12.0),
-          child: Text(
-            name,
-            style: TextStyle(color: Colors.grey),
-          ),
-        ),
-        TextField(
-          cont
+      "bio": b
                 )
               )
             ],
