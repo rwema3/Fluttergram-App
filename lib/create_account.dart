6b8068f274bcb8483e71161a345f616540fe9e3e
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-15 04:48:14 -0700

    child: Container(       decoration: BoxDecoration(           border: Border.all(width: 1.0, color: Colors.black26),           borderRadius: BorderRadius.circular(7.0)),       child: TextField(         controller: name,         decoration: InputDecoration(             border: InputBorder.none,             contentPadding: const EdgeInsets.all(10.0),             labelText: "Username",             labelStyle: TextStyle(fontSize: 15.0)),       ),     ),   ),

--- lib/create_account.dart
+++ lib/create_account.dart
@@ -4,20 +4,7 @@ class CreateAccount extends StatefulWidget {
   @override
   _CreateAccountState createState() => _CreateAccountState();
 }
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(width: 1.0, color: Colors.black26),
-              borderRadius: BorderRadius.circular(7.0)),
-          child: TextField(
-            controller: name,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                contentPadding: const EdgeInsets.all(10.0),
-                labelText: "Username",
-                labelStyle: TextStyle(fontSize: 15.0)),
-          ),
-        ),
-      ),
+    
       GestureDetector(
         onTap: () {
           if (name.text == null || name.text.length == 0){
