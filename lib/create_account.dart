7fc42723d28c3d320fd8927ce024090827c75e31
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-15 04:47:59 -0700

    offset

--- lib/create_account.dart
+++ lib/create_account.dart
@@ -4,32 +4,6 @@ class CreateAccount extends StatefulWidget {
   @override
   _CreateAccountState createState() => _CreateAccountState();
 }
-
-class _CreateAccountState extends State<CreateAccount> {
-  final name = TextEditingController();
-
-
-  @override
-  void dispose() {
-    // Clean up the controller when the Widget is removed from the Widget tree
-    name.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context2) {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.only(top: 25.0),
-        child: Center(
-          child: Text(
-            "Create a username",
-            style: TextStyle(fontSize: 25.0),
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(16.0),
         child: Container(
           decoration: BoxDecoration(
               border: Border.all(width: 1.0, color: Colors.black26),
