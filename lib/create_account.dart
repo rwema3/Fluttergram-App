5c48cca9462544eefe45bb88d00ae3e9c3880776
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-16 22:50:16 -0700

    tm

--- lib/create_account.dart
+++ lib/create_account.dart
@@ -9,6 +9,12 @@ class _CreateAccountState extends State<CreateAccount> {
   final name = TextEditingController();
 
 
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is removed from the Widget tree
+    name.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context2) {
